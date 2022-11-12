9ddbe1ee116abfa7385a69882b846da509ffd34e
maqingjie <15234447741@163.com>
2022-11-12 16:54:06 +0800

    docs: 日志

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -5,6 +5,7 @@ describe('webpack.base.js test case:', () => {
 
   const baseConfig = require('../../lib/webpack.base')
 
+  console.log(baseConfig)
   it('entry', () => {
     // F:/code/webpack/code/my-project/builder-webpack/test/smoke/template/src/index/index.js
     accert.equal(baseConfig.entry.index.indexOf('ma-builder-webpack-test/test/smoke/template/src/index/index.js') > -1, true)
